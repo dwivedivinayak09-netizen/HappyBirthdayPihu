863c4936d8cabe8e195c93c1e2df035b3f992ff1
dwivedivinayak09-netizen <dwivedivinayak09@gmail.com>
2026-06-16 03:25:35 +0530

    Update cause.js

--- cause.js
+++ cause.js
@@ -5,37 +5,20 @@ const reasons = [
         gif: "gif1.gif"
     },
     {
-        text: "Because even after knowing my feelings, you never changed. 💖",
+        text: "Because even after knowing my feelings, you never changed. You stayed just as kind, warm and understanding as before. 💖",
         emoji: "🌸",
         gif: "gif2.gif"
     },
     {
-        text: "Because you care in ways you don't always say. ✨",
+        text: "Because you care in ways you don't always say out loud, and somehow I always feel it. ✨",
         emoji: "💗",
         gif: "gif1.gif"
+    },
+    {
+        text: "Because whenever I think about the best unexpected things that happened in my life, you are always on that list. 🦋❤️",
+        emoji: "🌷",
+        gif: "gif2.gif"
     }
 ];
 
-let i = 0;
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const button = document.querySelector(".shuffle-button");
-    const container = document.getElementById("reasons-container");
-
-    button.addEventListener("click", function () {
-
-        container.innerHTML = `
-            <h2>${reasons[i].emoji}</h2>
-            <p>${reasons[i].text}</p>
-            <img src="${reasons[i].gif}" width="150">
-        `;
-
-        i++;
-
-        if (i >= reasons.length) {
-            i = 0;
-        }
-    });
-
-});
+cause.js h
